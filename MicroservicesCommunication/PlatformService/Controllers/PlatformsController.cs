6526901a1768ce91b2cd86c5ac98e24ea8dfcfae
fabioono25 <fabioono25@gmail.com>
2022-09-09 09:17:16 +0200

    Creating Controller + mapping DTOs and model using AutoMapper

---
+++ MicroservicesCommunication/PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetAllPlatforms()
+        {
+            Console.WriteLine("Getting Pplatforms...");
+
+            var platforms = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformsById")]
+        public ActionResult<PlatformReadDto> GetPlatformsById(int id)
+        {
+            Console.WriteLine($"Getting Platform by Id {id}...");
+
+            var platform = _repository.GetPlatformById(id);
+
+            if (platform == null)
+                return NotFound();
+
+            return Ok(_mapper.Map<PlatformReadDto>(platform));
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            Console.WriteLine($"Creating a Pplatform: {platformCreateDto.Name}...");
+
+            var platform = _mapper.Map<Platform>(platformCreateDto);
+
+            _repository.CreatePlatform(platform);
+            _repository.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platform);
+
+            return CreatedAtRoute(nameof(GetPlatformsById) , new { Id = platformReadDto.Id }, platformReadDto);
+        }
+    }
+}
---
+++ MicroservicesCommunication/PlatformService/Profiles/PlatformProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles
+{
+    public class PlatformProfile : Profile
+    {
+        public PlatformProfile()
+        {
+            // source --> target
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatformCreateDto, Platform>();
+        }
+    }
+}
+
