057509eedc367f98e2e0328ac1a95c28792ed5a8
Fabio Ono Tavoloni <fabioono25@gmail.com>
2022-10-23 17:09:58 +0200

    CatalogAPI bootstrapping

---
+++ MicroservicesEcommerce/src/Services/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    // the contract that contains the method to manipulate the DB
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
+
